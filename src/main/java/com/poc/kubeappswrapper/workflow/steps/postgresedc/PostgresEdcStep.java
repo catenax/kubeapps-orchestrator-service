7029bf69f2877bec9eec439ade97868fe42aab59
Dmitrii <dmitrii.vasiunin@t-systems.com>
2022-08-25 14:54:58 +0300

    start working on postgres step

---
+++ src/main/java/com/poc/kubeappswrapper/workflow/steps/postgresedc/PostgresEdcStep.java
@@ -0,0 +1,10 @@
+package com.poc.kubeappswrapper.workflow.steps.postgresedc;
+
+import com.poc.kubeappswrapper.workflow.Task;
+
+public class PostgresEdcStep extends Task {
+    @Override
+    public void run() {
+
+    }
+}
