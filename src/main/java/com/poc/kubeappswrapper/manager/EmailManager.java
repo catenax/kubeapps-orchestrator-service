bdd5d4b7ab1fb01aecaaf0f24c198d765208177a
Sankalp Kedari <sankalp.kedari@t-systems.com>
2022-09-02 10:46:50 +0530

    Added code to send an email after succesful deployment of DFT

---
+++ src/main/java/com/poc/kubeappswrapper/manager/EmailManager.java
@@ -0,0 +1,60 @@
+package com.poc.kubeappswrapper.manager;
+
+import com.poc.kubeappswrapper.model.EmailRequest;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Date;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailManager {
+
+    @Autowired
+    private MimeMessage mimeMessage;
+
+    @Value("${mail.from.address}")
+    private String fromEmail;
+
+    @Value("${mail.to}")
+    private String toEmail;
+
+    final Configuration configuration;
+
+    public EmailManager(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public ResponseEntity<String> sendEmail(EmailRequest emailRequest) {
+        try {
+
+            mimeMessage.setFrom(new InternetAddress(fromEmail));
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(toEmail));
+            mimeMessage.setSubject(emailRequest.getSubject());
+
+            String data = getEmailContent(emailRequest);
+            mimeMessage.setContent(data, "text/html; charset=utf-8"); // as "text/plain"
+            mimeMessage.setSentDate(new Date());
+            Transport.send(mimeMessage);
+            return new ResponseEntity<>("Email Sent Success", HttpStatus.OK);
+        } catch (MessagingException | IOException | TemplateException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    String getEmailContent(EmailRequest emailRequest) throws IOException, TemplateException {
+        StringWriter stringWriter = new StringWriter();
+        configuration.getTemplate(emailRequest.getTemplateFileName()).process(emailRequest.getEmailContent(), stringWriter);
+        return stringWriter.getBuffer().toString();
+    }
+}
