0253a3e9b25f9a3ba03e1d2de9fa6a92d0641f02
Aniket Sonawane <aniket.sonawane@t-systems.com>
2022-09-15 11:31:01 +0530

    Code refractor for daps wrapper

--- src/main/java/com/poc/kubeappswrapper/manager/DAPsWrapperManager.java
+++ src/main/java/com/poc/kubeappswrapper/manager/DAPsWrapperManager.java
@@ -3,27 +3,19 @@ package com.poc.kubeappswrapper.manager;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.cert.X509Certificate;
 import java.util.Map;
 import java.util.UUID;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poc.kubeappswrapper.constant.TriggerStatusEnum;
 import com.poc.kubeappswrapper.entity.AutoSetupTriggerDetails;
 import com.poc.kubeappswrapper.entity.AutoSetupTriggerEntry;
 import com.poc.kubeappswrapper.exception.ServiceException;
 import com.poc.kubeappswrapper.model.CustomerDetails;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.*;
 import org.springframework.retry.annotation.Backoff;
@@ -33,10 +25,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-
-import com.poc.kubeappswrapper.utility.Certutil;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Service
 @Slf4j
@@ -64,24 +52,6 @@ public class DAPsWrapperManager implements Serializable{
         this.autoSetupTriggerManager = autoSetupTriggerManager;
     }
 
-
-    public HttpStatus createClient1(X509Certificate certificate, String token) throws IOException {
-		
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setBearerAuth("");
-        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-        body.add("clientName", Certutil.getClientId(certificate));
-        body.add("file", new ByteArrayResource(Certutil.getAsString(certificate).getBytes()));
-        HttpEntity<MultiValueMap<String, Object>> requestEntity
-                = new HttpEntity<>(body, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        var result = restTemplate.postForEntity(dapsRegistrationUrl, requestEntity, String.class);
-        return result.getStatusCode();
-        
-    }
-
     @Retryable(value = { ServiceException.class }, maxAttemptsExpression = "${retry.maxAttempts}", backoff = @Backoff(delayExpression = "${retry.backOffDelay}"))
     public Map<String, String> createClient(CustomerDetails customerDetails, Map<String, String> inputData,
                                     AutoSetupTriggerEntry triger) throws IOException {
@@ -96,28 +66,23 @@ public class DAPsWrapperManager implements Serializable{
         headers.setContentType(MediaType.MULTIPART_FORM_DATA);
         headers.setBearerAuth(getKeycloakToken());
         try {
-
+        Path file = getTestFile(inputData.get("selfsigncertificate"));
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         body.add("clientName", inputData.get("dapsclientid"));
-
-        body.add("file", getTestFile(inputData.get("selfsigncertificate")));
+        body.add("file", new FileSystemResource(file.toFile()));
         HttpEntity<MultiValueMap<String, Object>> requestEntity
                 = new HttpEntity<>(body, headers);
 
         RestTemplate restTemplate = new RestTemplate();
-            System.out.println("Path : "+dapsRegistrationUrl);
         var result = restTemplate.postForEntity(dapsRegistrationUrl, requestEntity, String.class);
-        log.info("Result : "+result);
         inputData.put("dapsurl", dapsurl);
         inputData.put("dapsjsksurl", dapsjsksurl);
         } catch (Exception ex) {
             log.error("DAPsWrapperManager failed retry attempt: : {}",
-                    RetrySynchronizationManager.getContext().getRetryCount() + 1);
-
+            RetrySynchronizationManager.getContext().getRetryCount() + 1);
             autoSetupTriggerDetails.setStatus(TriggerStatusEnum.FAILED.name());
             autoSetupTriggerDetails.setRemark(ex.getMessage());
             throw new ServiceException("DAPsWrapperManager Oops! We have an exception - " + ex.getMessage());
-
         } finally {
             autoSetupTriggerManager.saveTriggerDetails(autoSetupTriggerDetails);
         }
@@ -141,17 +106,14 @@ public class DAPsWrapperManager implements Serializable{
                 ).build();
 
         keycloak.tokenManager().getAccessToken();
-        System.out.println("Keycloak token : "+keycloak.tokenManager().getAccessTokenString());
-
         return keycloak.tokenManager().getAccessTokenString();
 
     }
 
-    public static FileSystemResource getTestFile(String str) throws IOException {
+    public static Path getTestFile(String str) throws IOException {
         Path testFile = Files.createTempFile("test-file1", ".crt");
-        System.out.println("Creating and Uploading Test File: " + testFile);
         Files.write(testFile, str.getBytes());
-        return new FileSystemResource(testFile.toFile());
+        return testFile;
     }
 
 }
