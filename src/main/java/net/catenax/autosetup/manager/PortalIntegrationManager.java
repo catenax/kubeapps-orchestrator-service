d5f150ced0135cc4929c5bd899e23fcbb8f9fd0b
Sachin Argade <sachin.argade@t-systems.com>
2022-10-06 23:52:48 +0530

    portal interface integration and manual conf. flag base

--- src/main/java/net/catenax/autosetup/manager/PortalIntegrationManager.java
+++ src/main/java/net/catenax/autosetup/manager/PortalIntegrationManager.java
@@ -20,13 +20,20 @@
 
 package net.catenax.autosetup.manager;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.keycloak.OAuth2Constants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import net.catenax.autosetup.portal.model.ServiceInstanceResultRequest;
+import net.catenax.autosetup.portal.model.ServiceInstanceResultResponse;
 import net.catenax.autosetup.portal.proxy.PortalIntegrationProxy;
 
 @Service
@@ -35,19 +42,78 @@ public class PortalIntegrationManager {
 
 	private final PortalIntegrationProxy portalIntegrationProxy;
 
+	@Value("${portal.url}")
+	private URI portalUrl;
+
+	@Value("${portal.keycloak.clientId}")
+	private String clientId;
+	
+	@Value("${portal.keycloak.clientSecret}")
+	private String clientSecret;
+	
+	@Value("${portal.keycloak.tokenURI}")
+	private URI tokenURI;
+	
+	
+	@Value("${portal.dft.keycloak.realm}")
+	private String keycloakRealm;
+	
+	@Value("${portal.dft.keycloak.clientId}")
+	private String keycloakClientId;
+	
+	@Value("${portal.dft.keycloak.url}")
+	private String keycloakUrl;
+	
+	@Value("${portal.dft.digitalTwinUrl}")
+	private String digitalTwinUrl;
+	
+	@Value("${portal.dft.digitalTwinAuthUrl}")
+	private String digitalTwinAuthUrl;
+	
+
 	@SneakyThrows
 	public Map<String, String> postServiceInstanceResultAndGetTenantSpecs(Map<String, String> inputData) {
 
 		String dftFrontendURL = inputData.get("dftFrontEndUrl");
 		String subscriptionId = inputData.get("subscriptionId");
+
+		Map<String, String> header = new HashMap<String, String>();
+		header.put("Authorization", "Bearer " + getKeycloakToken());
+
+		ServiceInstanceResultRequest serviceInstanceResultRequest = ServiceInstanceResultRequest.builder()
+				.requestId(subscriptionId).offerUrl(dftFrontendURL).build();
+
+		ServiceInstanceResultResponse serviceInstanceResultResponse = portalIntegrationProxy
+				.postServiceInstanceResultAndGetTenantSpecs(portalUrl, header, serviceInstanceResultRequest);
 		
-		ServiceInstanceResultRequest serviceInstanceResultRequest = ServiceInstanceResultRequest
-				.builder()
-				.requestId(subscriptionId)
-				.appUrl(dftFrontendURL)
-				.build();
-		portalIntegrationProxy.postServiceInstanceResultAndGetTenantSpecs(serviceInstanceResultRequest);
+		inputData.put("digital-twins.hostname", digitalTwinUrl);
+		inputData.put("digital-twins.authentication.url", digitalTwinAuthUrl);
+		inputData.put("digital-twins.authentication.clientId", serviceInstanceResultResponse.getTechnicalUserInfo().getTechnicalUserId());
+		inputData.put("digital-twins.authentication.clientSecret",
+				serviceInstanceResultResponse.getTechnicalUserInfo().getTechnicalUserSecret());
+
+		inputData.put("dftkeycloakurl", keycloakUrl);
+		inputData.put("dftcloakrealm", keycloakRealm);
+		inputData.put("dftbackendkeycloakclientid", keycloakClientId);
+		inputData.put("dftfrontendkeycloakclientid", keycloakClientId);
+		
 		return inputData;
+	}
+
+	@SneakyThrows
+	private String getKeycloakToken() {
+		
+		MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		body.add(OAuth2Constants.GRANT_TYPE, OAuth2Constants.CLIENT_CREDENTIALS);
+		body.add(OAuth2Constants.CLIENT_ID, clientId);
+		body.add(OAuth2Constants.CLIENT_SECRET, clientSecret);
+		var resultBody = portalIntegrationProxy.readAuthToken(tokenURI, body);
+
+		if (resultBody != null) {
+			return resultBody.getAccessToken();
+		}
+		return null;
+
 	}
 
 }
