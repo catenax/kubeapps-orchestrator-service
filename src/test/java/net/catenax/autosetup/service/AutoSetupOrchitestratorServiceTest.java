a1815f9914d52e3e1fc42dea7e80aa4f915f3876
Sankalp Kedari <sankalp.kedari@t-systems.com>
2022-10-07 18:10:28 +0530

    Corrected the test

--- src/test/java/net/catenax/autosetup/service/AutoSetupOrchitestratorServiceTest.java
+++ src/test/java/net/catenax/autosetup/service/AutoSetupOrchitestratorServiceTest.java
@@ -2,13 +2,18 @@ package net.catenax.autosetup.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.catenax.autosetup.manager.AutoSetupTriggerManager;
 import net.catenax.autosetup.model.AutoSetupRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
@@ -25,12 +30,15 @@ class AutoSetupOrchitestratorServiceTest {
     @Autowired
     private AutoSetupOrchitestratorService autoSetupOrchitestratorService;
 
+    @MockBean
+    private AutoSetupTriggerManager autoSetupTriggerManager;
+
     @Test
     void createPackage() {
 
         String json = "{\n" +
                 "    \"customer\": {\n" +
-                "        \"organizationName\": \"Verul\",\n" +
+                "        \"organizationName\": \"Verul1\",\n" +
                 "        \"country\": \"IN\",\n" +
                 "        \"state\": \"GN\",\n" +
                 "        \"city\": \"BL\",\n" +
@@ -40,12 +48,13 @@ class AutoSetupOrchitestratorServiceTest {
                 "        \"bpnNumber\": \"BPN12345611\",\n" +
                 "        \"role\": \"recycler\",\n" +
                 "        \"subscriptionId\": \"DAS-D234\",\n" +
-                "        \"serviceId\": \"T-SYSTEM-DFT-EDC\"\n" +
+                "        \"serviceId\": \"DFT-WITH-EDC\"\n" +
                 "    }\n" +
                 "}";
 
         try {
             AutoSetupRequest autoSetupRequest = new ObjectMapper().readValue(json,AutoSetupRequest.class);
+            Mockito.when(autoSetupTriggerManager.isAutoSetupAvailableforOrgnizationName(Mockito.anyString())).thenReturn(null);
             String uuid = autoSetupOrchitestratorService.createPackage(autoSetupRequest);
             assertThat(uuid).isNotEmpty();
 
